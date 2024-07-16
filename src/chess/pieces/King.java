f99cee63922083dcd48601ef790bd1c938f1e823
Lucas-LDev <lucasunisinos0@gmail.com>
2024-07-16 16:34:32 -0300

    Updated classes: King and Rook (Implementing possible moves of king)

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -12,6 +13,70 @@ public class King extends ChessPiece {
     @Override
     public String toString() {
         return "K";
+    }
+
+    //Especial methods
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        //Above
+        p.setValues(position.getRow() -1, position.getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Left
+        p.setValues(position.getRow(), position.getColumn() -1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Right
+        p.setValues(position.getRow(), position.getColumn() +1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Bellow
+        p.setValues(position.getRow() +1, position.getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Northwest
+        p.setValues(position.getRow() -1, position.getColumn() -1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //North East
+        p.setValues(position.getRow() -1, position.getColumn() +1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //South-west
+        p.setValues(position.getRow() +1, position.getColumn() -1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Southeast
+        p.setValues(position.getRow() +1, position.getColumn() +1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
     }
 
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -17,7 +17,7 @@ public class Rook extends ChessPiece {
     }
 
     //Especial methods
-
+    @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
