17eb052a7f1b226bdac1b3612289fb6c05a99619
Lucas-LDev <lucasunisinos0@gmail.com>
2024-07-16 11:08:14 -0300

    Created classes: ChessException and ChessPosition | Updated class: ChessMatch

---
+++ src/chess/ChessException.java
@@ -0,0 +1,13 @@
+package chess;
+
+import java.io.Serial;
+
+public class ChessException extends  RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
