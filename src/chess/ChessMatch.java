f3542553284b8830e2e18efe2b442f9c3b3f4c9c
Lucas-LDev <lucasunisinos0@gmail.com>
2024-07-16 10:27:13 -0300

    Created classes: King and Rook | Updated classes: Board and ChessMatch

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,12 +2,16 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         this.board = new Board(8,8);
+        initialSetup();
     }
 
     //Especial methods
@@ -21,5 +25,12 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+        board.placePiece(new Rook(board, Color.BLACK), new Position(7,4));
+
     }
 }
